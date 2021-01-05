c74ec1e9df59d02f9e2f3de2e401dd86da703761
klymovy4 <klymovy4roman@gmail.com>
2021-01-05 19:17:05 +0200

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h2>Home Page</h2>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
